8f12d80a9e08834d4b760beea39c10198c3751c7
Nv22058 <154870326+Nv22058@users.noreply.github.com>
2024-05-18 19:52:37 +0000

    29 - creating Post, Comment, and Categories model

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,105 +1,84 @@
-
-Filter changed files
- 99 changes: 95 additions & 4 deletions99  
-controllers/userControllers.js
-@@ -1,6 +1,8 @@
 import { uploadPicture } from "../middleware/uploadPictureMiddleware";
-import User from "../models/User";	import User from "../models/User";
+import User from "../models/User";
 import { fileRemover } from "../utils/fileRemover";
-
-
-export const registerUser = async (req, res, next) => {	const registerUser = async (req, res, next) => {
-  try {	  try {
-    const { name, email, password } = req.body;	    const { name, email, password } = req.body;
-
-
-    // check whether the user exists or not	    // check whether the user exists or not
-    let user = await User.findOne({ email });	    let user = await User.findOne({ email });
-    if (user) {	    if (user) {
-      throw new Error("User have already registered");	      throw new Error("User have already registered");
-    }	    }
-    // creating a new user	    // creating a new user
-    user = await User.create({	    user = await User.create({
-      name,	      name,
-      email,	      email,
-      password,	      password,
-    });	    });
-    return res.status(201).json({	    return res.status(201).json({
-      _id: user._id,	      _id: user._id,
-      avatar: user.avatar,	      avatar: user.avatar,
-      name: user.name,	      name: user.name,
-      email: user.email,	      email: user.email,
-      verified: user.verified,	      verified: user.verified,
-      admin: user.admin,	      admin: user.admin,
-      token: await user.generateJWT(),	      token: await user.generateJWT(),
-    });	    });
-  } catch (error) {	  } catch (error) {
-    next(error);	    next(error);
-  }	  }
-};	};
-
-
-export const loginUser = async (req, res, next) => {	const loginUser = async (req, res, next) => {
-  try {	  try {
-    const { email, password } = req.body;	    const { email, password } = req.body;
-
-
-    let user = await User.findOne({ email });	    let user = await User.findOne({ email });
-    if (!user) {	    if (!user) {
-      throw new Error("Email not found");	      throw new Error("Email not found");
-    }	    }
-    if (await user.comparePassword(password)) {	    if (await user.comparePassword(password)) {
-      return res.status(201).json({	      return res.status(201).json({
-        _id: user._id,	        _id: user._id,
-        avatar: user.avatar,	        avatar: user.avatar,
-        name: user.name,	        name: user.name,
-        email: user.email,	        email: user.email,
-        verified: user.verified,	        verified: user.verified,
-        admin: user.admin,	        admin: user.admin,
-        token: await user.generateJWT(),	        token: await user.generateJWT(),
-      });	      });
-    } else {	    } else {
-      throw new Error("Invalid email or password");	      throw new Error("Invalid email or password");
-    }	    }
-  } catch (error) {	  } catch (error) {
-    next(error);	    next(error);
-  }	  }
-};	};
-
-
-export const userProfile = async (req, res, next) => {	const userProfile = async (req, res, next) => {
-  try {	  try {
-    let user = await User.findById(req.user._id);	    let user = await User.findById(req.user._id);
-
-
-    if (user) {	    if (user) {
-      return res.status(201).json({	      return res.status(201).json({
-        _id: user._id,	        _id: user._id,
-        avatar: user.avatar,	        avatar: user.avatar,
-        name: user.name,	        name: user.name,
-        email: user.email,	        email: user.email,
-        verified: user.verified,	        verified: user.verified,
-        admin: user.admin,	        admin: user.admin,
-      });	      });
-    } else {	    } else {
-      let error = new Error("User not found");	      let error = new Error("User not found");
-      error.statusCode = 404;	      error.statusCode = 404;
-      next(error);	      next(error);
-    }	    }
-  } catch (error) {	  } catch (error) {
-    next(error);	    next(error);
-  }	  }
-};	};
-
-
-export { registerUser, loginUser };	const updateProfile = async (req, res, next) => {
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    // check whether the user exists or not
+    let user = await User.findOne({ email });
+    if (user) {
+      throw new Error("User have already registered");
+    }
+    // creating a new user
+    user = await User.create({
+      name,
+      email,
+      password,
+    });
+    return res.status(201).json({
+      _id: user._id,
+      avatar: user.avatar,
+      name: user.name,
+      email: user.email,
+      verified: user.verified,
+      admin: user.admin,
+      token: await user.generateJWT(),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("Email not found");
+    }
+    if (await user.comparePassword(password)) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+        token: await user.generateJWT(),
+      });
+    } else {
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const userProfile = async (req, res, next) => {
   try {
     let user = await User.findById(req.user._id);
-
+    if (user) {
+      return res.status(201).json({
+        _id: user._id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+      });
+    } else {
+      let error = new Error("User not found");
+      error.statusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+const updateProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
     if (!user) {
       throw new Error("User not found");
     }
-
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
     if (req.body.password && req.body.password.length < 6) {
@@ -107,9 +86,7 @@ export { registerUser, loginUser };	const updateProfile = async (req, res, next)
     } else if (req.body.password) {
       user.password = req.body.password;
     }
-
     const updatedUserProfile = await user.save();
-
     res.json({
       _id: updatedUserProfile._id,
       avatar: updatedUserProfile.avatar,
@@ -123,25 +100,27 @@ export { registerUser, loginUser };	const updateProfile = async (req, res, next)
     next(error);
   }
 };
-
 const updateProfilePicture = async (req, res, next) => {
   try {
     const upload = uploadPicture.single("profilePicture");
 
     upload(req, res, async function (err) {
       if (err) {
-        const error = new Error("An unknown error occured when uploading " + err.message);
+        const error = new Error(
+          "An unknown error occured when uploading " + err.message
+        );
         next(error);
       } else {
         // every thing went well
         if (req.file) {
-          const updatedUser = await User.findByIdAndUpdate(
-            req.user._id,
-            {
-              avatar: req.file.filename,
-            },
-            { new: true }
-          );
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          if (filename) {
+            fileRemover(filename);
+          }
+          updatedUser.avatar = req.file.filename;
+          await updatedUser.save();
           res.json({
             _id: updatedUser._id,
             avatar: updatedUser.avatar,
@@ -174,7 +153,6 @@ const updateProfilePicture = async (req, res, next) => {
     next(error);
   }
 };
-
 export {
   registerUser,
   loginUser,
